e542dae2e7ed9f5ecddbd483154049098f8a36e5
ridel1e <ridellee@gmail.com>
2017-03-14 21:52:12 +0300

    base model added

---
+++ app/core/base-model.js
@@ -0,0 +1,47 @@
+
+/**
+ * Base model class
+ * @class
+ */
+class BaseModel {
+  /**
+   * @constructs
+   * @param {Object} attributes entity attributes
+   */
+  constructor (attributes) {
+    const sanitizedAttributes = this._sanitize(attributes);
+
+    this._attributes = sanitizedAttributes;
+  }
+
+  get (key) {
+    return this._attributes[key];
+  }
+
+  set (key, value) {
+    this._attributes[key] = value;
+  }
+
+  toObject () {
+    return Object.assign({}, this._attributes);
+  }
+
+  /**
+   * Returns sanitized attributes
+   * @private 
+   * @param {Object} attributes raw attributes
+   * @return {Object} sanitized attributes
+   */
+  _sanitize (attributes) {
+    return Object.assign({}, this._modelScheme, attributes);
+  }
+
+  /**
+   * @abstract
+   */
+  get _modelScheme () {
+    return {};
+  }  
+}
+
+export { BaseModel }
