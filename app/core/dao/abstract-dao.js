6408e4e8880557e953fde0b09b32a994728f8a73
ridel1e <ridellee@gmail.com>
2017-03-14 23:33:40 +0300

    abstract data access object added

--- app/core/dao/abstract-dao.js
+++ app/core/dao/abstract-dao.js
@@ -9,14 +9,34 @@ class AbstractDAO {
    * @constructs
    */
   constructor () {}
+  
+  findAll () {
+    return 
+      _onFindAll()
+      .then((rawData) => 
+        rawData.map((item) => new this.Entity(item)));
+  }
 
-  findAll () {}
+  findBy (params) {
+    return 
+      _onFindBy(params)
+      .then((rawData) => 
+        rawData.map((item) => new this.Entity(item)));
+  }
 
-  findBy () {}
+  findById (id) {
+    return
+      _onFindById(id)
+      .then((rawData) => 
+        new this.Entity(rawData));
+  }
 
-  findById () {}
-
-  findOneBy () {}
+  findOneBy (params) {
+    return
+      _onFindById(params)
+      .then((rawData) => 
+        new this.Entity(rawData));
+  }
 
   save (object) {
     if (!(object instanceof this.Entity )) {
@@ -25,7 +45,10 @@ class AbstractDAO {
       throw new Error(error);
     }
 
-    console.log(object);
+    return
+      _onSave(object)
+      .then((id) => 
+        object.set('id', id));
   }
 
   remove (object) {
@@ -34,12 +57,59 @@ class AbstractDAO {
         `Entity has incorrect type. "${this.Entity.name}" expected`;
       throw new Error(error);
     }
+
+    return
+      _onRemove(object)
   }
 
+  /* abstract methods */
+
   /**
-   * @abstract
+   * On find all function
+   * @abstract 
+   * @returns {promise} 
    */
-  get _conntection () { return {}; }
+  _onFindAll () { return Promise.resolve() }
+
+  /**
+   * On find by function
+   * @abstract 
+   * @param {Object} params query params
+   * @returns {promise} 
+   */
+  _onFindBy (params) { return Promise.resolve() }
+
+  /**
+   * On find by id function
+   * @abstract
+   * @param {number|string} id entity id
+   * @returns {promise} 
+   */
+  _onFindById (id) { return Promise.resolve() }
+
+  /**
+   * On find one by function
+   * @abstract
+   * @param {Object} params query params 
+   * @returns {promise} 
+   */
+  _onFindOneBy (params) { return Promise.resolve() }
+
+  /**
+   * On save function
+   * @abstract 
+   * @param {Object} object entity object
+   * @returns {promise} 
+   */
+  _onSave (object) { return Promise.resolve() }
+
+  /**
+   * On remove function
+   * @abstract 
+   * @param {Object} object entity object
+   * @returns {promise} 
+   */
+  _onRemove (object) { return Promise.resolve() }
 
   get Entity () { return {}; } 
 }
